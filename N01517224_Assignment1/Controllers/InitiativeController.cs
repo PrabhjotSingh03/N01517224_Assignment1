d5d35c604ff634b1ae7808ff0e761d46bdf41c37
Prabhjot Singh Sains <prabhjot.sains@gmail.com>
2023-02-13 13:29:23 -0500

    Update InitiativeController.cs

--- N01517224_Assignment1/Controllers/InitiativeController.cs
+++ N01517224_Assignment1/Controllers/InitiativeController.cs
@@ -15,7 +15,7 @@ namespace N01517224_Assignment1.Controllers
             double fortnights = id / 14;                                //Step-1 Id Divided by 14 
             int fortnights2 = Convert.ToInt32(fortnights + 1);          //Step-2 Division value from step-1 gets added here
             double fortnightCharge = 5.50 * fortnights2;                //Step-3  Value generated from step-2 gets multiplied in this step
-            double HstTotal = fortnights2 * 0.72;                       //step-4 Value from step-2 gets multiplied here
+            double HstTotal = fortnights2 * 0.13;                       //step-4 Value from step-2 gets multiplied here
             double TotalBill = fortnightCharge + HstTotal;              //Step-5 Values from step-3 and step-4 gets added here.
             return new string[]
                 {
